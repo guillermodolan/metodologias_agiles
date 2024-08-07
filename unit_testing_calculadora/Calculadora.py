e4e9f1676aa982bea9889e60e7d0c2974422bbaf
Guillermo Dolan <guillermo230899@gmail.com>
2024-08-07 20:05:18 -0300

    Primer práctica finalizada

---
+++ unit_testing_calculadora/Calculadora.py
@@ -0,0 +1,13 @@
+# Primer actividad de práctica, realizada el 06/08/2024
+
+class Calculadora:
+    numero1 = 0
+    numero2 = 0
+
+    @staticmethod
+    def sumar():
+        return Calculadora.numero1 + Calculadora.numero2
+
+    @staticmethod
+    def restar():
+        return Calculadora.numero1 - Calculadora.numero2
---
+++ unit_testing_calculadora/testing_file.py
@@ -0,0 +1,27 @@
+# Primer actividad de práctica, realizada el 06/08/2024
+
+
+import unittest
+
+from Calculadora import Calculadora
+
+
+class TestSuma(unittest.TestCase):
+
+    def test_suma_uno_mas_tres(self):
+        # Este caso va a ser exitoso
+        Calculadora.numero1 = 1
+        Calculadora.numero2 = 3
+        self.assertEqual(Calculadora.sumar(), 4)
+
+    def test_resta_ocho_menos_cinco(self):
+        # Este caso va a ser erróneo
+        Calculadora.numero1 = 8
+        Calculadora.numero2 = 5
+        # El resultado debería ser 3, pero vamos a simular que es 5
+        self.assertEqual(Calculadora.restar(), 5)
+
+
+
+if __name__ == 'main':
+    unittest.main()
