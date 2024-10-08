993a3e1c45230e62be31c3d35d1516e6a5fde886
Guillermo <guillermo230899@gmail.com>
2024-10-08 19:35:15 -0300

    Actualización Sprint 3

--- tpi_ahorcado/tests.py
+++ tpi_ahorcado/tests.py
@@ -4,7 +4,8 @@ from Ahorcado import Ahorcado
 class Test(unittest.TestCase):
     
     letra = 'a'
-    palabra = 'prueba'
+    palabra_correcta = 'prueba'
+    palabra_incorrecta = 'nula'
 
     def test_acierta_letra(self):
         self.assertEqual(Ahorcado.ingresar_letra(Test.letra), 'a')
@@ -20,8 +21,11 @@ class Test(unittest.TestCase):
         Ahorcado.vidas = 1
         self.assertEqual(Ahorcado.descuenta_vida(), 'Perdiste')
         
-    def test_acierta_palabra(self):
-        self.assertEqual(Ahorcado.ingresar_palabra(Test.palabra), 'prueba')
+    def test_acierta_palabra_correcta(self):
+        self.assertEqual(Ahorcado.ingresar_palabra(Test.palabra_correcta), Test.palabra_correcta)
+        
+    def test_falla_palabra_correcta(self):
+        self.assertNotEqual(Ahorcado.ingresar_palabra(Test.palabra_incorrecta), Test.palabra_correcta)
     
     
 if __name__ ==  '__main__':
