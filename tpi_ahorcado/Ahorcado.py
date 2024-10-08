24fdb28e47ae44d821f80d9b4be716ce1323f432
Guillermo <guillermo230899@gmail.com>
2024-10-08 19:23:34 -0300

    Sprint 3

--- tpi_ahorcado/Ahorcado.py
+++ tpi_ahorcado/Ahorcado.py
@@ -1,13 +1,20 @@
 class Ahorcado:
     
     # Definimos 6 vidas (cabeza, tronco, brazos y piernas)
-    vidas = 6
+    vidas = 0
     
     @staticmethod
     def ingresar_letra(letra):
         return letra
     
     @staticmethod
+    def ingresar_palabra(palabra):
+        return palabra
+    
+    @staticmethod
     def descuenta_vida():
         Ahorcado.vidas -= 1
-        return Ahorcado.vidas
+        if Ahorcado.vidas != 0:
+            return Ahorcado.vidas
+        else:
+            return 'Perdiste'
--- tpi_ahorcado/tests.py
+++ tpi_ahorcado/tests.py
@@ -4,15 +4,25 @@ from Ahorcado import Ahorcado
 class Test(unittest.TestCase):
     
     letra = 'a'
+    palabra = 'prueba'
 
     def test_acierta_letra(self):
         self.assertEqual(Ahorcado.ingresar_letra(Test.letra), 'a')
         
     def test_falla_letra(self):
         self.assertNotEqual(Ahorcado.ingresar_letra(Test.letra), '')
+        
+    def test_falla_letra_descuenta_vida(self):
+        Ahorcado.vidas = 6
+        self.assertEqual(Ahorcado.descuenta_vida(), 5)
+        
+    def test_fin_de_vidas(self):
+        Ahorcado.vidas = 1
+        self.assertEqual(Ahorcado.descuenta_vida(), 'Perdiste')
+        
+    def test_acierta_palabra(self):
+        self.assertEqual(Ahorcado.ingresar_palabra(Test.palabra), 'prueba')
     
     
-
-
 if __name__ ==  '__main__':
     unittest.main()  # run all tests
