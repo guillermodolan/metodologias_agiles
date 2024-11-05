70734eb3adf317ece546e2f22048e6010f5851e1
Guillermo <guillermo230899@gmail.com>
2024-11-05 20:46:48 -0300

    Lógica de juego avanzada

--- tpi_ahorcado/Ahorcado.py
+++ tpi_ahorcado/Ahorcado.py
@@ -1,17 +1,30 @@
 class Ahorcado:
     
-    # Definimos 6 vidas (cabeza, tronco, brazos (2) y piernas (2) )
+    # Definimos 6 vidas (cabeza, tronco, brazos (2) y piernas (2))
     vidas = 6
 
     def __init__(self, palabra):
         self.palabra = palabra
+        self.letras_adivinadas = []
+        self.letras_erradas = []
     
     def ingresar_letra(self, letra):
-        if letra in self.palabra:
-            return True
+        if letra in self.letras_adivinadas or letra in self.letras_erradas:
+            print('Ya has ingresado esa letra. Por favor ingresá otra.')
         else:
-            Ahorcado.descuenta_vida()
-            return False
+            if letra in self.palabra:
+                self.letras_adivinadas.append(letra)
+                self.muestra_palabra()
+                print('Acertaste. ¡Continúa así!')
+                return True
+            else:
+                self.letras_erradas.append(letra)
+                print(f'Letras erradas: {self.letras_erradas}')
+                print(f'Perdiste una vida.')
+                Ahorcado.descuenta_vida()
+                print(f'Te quedan {Ahorcado.vidas} vidas')
+                return False
+            
     
     def arriesgar_palabra(self, palabra_ingresada):
         if not self.palabra == palabra_ingresada:
@@ -30,3 +43,12 @@ class Ahorcado:
     @staticmethod
     def adivina_palabra():
         return 'Ganaste'
+
+    def muestra_palabra(self):
+        palabra_actual = ''
+        for letra in self.palabra:
+            if letra in self.letras_adivinadas:
+                palabra_actual = palabra_actual + letra
+            else:
+                palabra_actual = palabra_actual + '_'
+        return palabra_actual
---
+++ tpi_ahorcado/main.py
@@ -0,0 +1,17 @@
+from Ahorcado import Ahorcado
+
+palabra_correcta = input('Ingrese la palabra correcta: ')
+ahorcado = Ahorcado(palabra_correcta)
+
+fin_juego = False
+while not fin_juego and Ahorcado.vidas > 0:
+    letra = input('Ingresá una letra: ')
+    ahorcado.ingresar_letra(letra)
+    print(f'Salida: {ahorcado.muestra_palabra()}')
+    if not '_' in ahorcado.muestra_palabra():
+        fin_juego = True
+        
+if Ahorcado.vidas != 0:
+    print('Fin del juego, ganaste')
+else:
+    print('Perdiste')
