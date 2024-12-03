aa4ce2eb665516a0a5ef861fe67b2f706e2b52e5
Guillermo <guillermo230899@gmail.com>
2024-12-03 20:02:40 -0300

    Modificación de Acceptance test

--- acceptance_tests.py
+++ acceptance_tests.py
@@ -17,6 +17,9 @@ class TestAhorcadoAcceptance(unittest.TestCase):
         service = Service(driver_path)
         options = webdriver.ChromeOptions()
         options.add_argument('--start-maximized')
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(service=service, options=options)
         self.driver.get("http://localhost:5000")
 
