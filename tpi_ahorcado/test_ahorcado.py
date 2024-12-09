f24c3c6fc53a8f09c19e2fd5e5c5ad5d0f7b7beb
Guillermo <guillermo230899@gmail.com>
2024-12-09 12:00:30 -0300

    Limpieza de código

--- tpi_ahorcado/test_ahorcado.py
+++ tpi_ahorcado/test_ahorcado.py
@@ -3,19 +3,21 @@ from app import app
 from Ahorcado import Ahorcado
 
 class Test(unittest.TestCase):
+    
+    """Clase implementada como módulo de tests de Back-End."""
 
     def setUp(self):
-        # Configuro el cliente de pruebas
         self.app = app.test_client()
         self.app.testing = True
 
     def test_index(self):
-        # Pruebo la ruta principal
+        """Test que verifica la ruta principal"""
         self.app.post('/configurar_palabra', data={'palabra': 'indice'})
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_configurar_palabra(self):
+        """Test que verifica que se configure una palabra"""
         response = self.app.post('/configurar_palabra', data={'palabra': 'python'})
         self.assertEqual(response.status_code, 200)
         json_data = response.get_json()
@@ -24,14 +26,15 @@ class Test(unittest.TestCase):
         self.assertEqual(json_data['erradas'], [])
 
     def test_acierta_letra(self):
-        # Compruebo que al ingresar una letra, esta sea válida desde la lógica
+        """Test que verifica que se acierte una letra"""
         palabra = 'prueba'
         ahorcado = Ahorcado(palabra)
         letra = 'a'
         self.assertTrue(ahorcado.ingresar_letra(letra))
 
     def test_acierta_letra_app(self):
-        # Compruebo que al ingresar una letra, esta sea válida desde la aplicación
+        """Test que verifica que se acierte una letra desde la 
+        aplicación"""
         self.app.post('/configurar_palabra', data={'palabra': 'prueba'})
         response = self.app.post('/ingresar_letra', data={'letra': 'a'})
         json_data = response.get_json()
@@ -40,13 +43,15 @@ class Test(unittest.TestCase):
         self.assertEqual(json_data['vidas'], 6)
 
     def test_falla_letra(self):
-        # Compruebo que al ingresar una letra que no está en la palabra, no se considere válido
+        """Test que verifica que falla una letra"""
         palabra = 'testea'
         ahorcado = Ahorcado(palabra)
         letra = 'g'
         self.assertFalse(ahorcado.ingresar_letra(letra))
 
     def test_falla_letra_app(self):
+        """Test que verifica que se descuente una vida al falla una letra
+        desde la aplicación"""
         self.app.post('/configurar_palabra', data={'palabra': 'testea'})
         response = self.app.post('/ingresar_letra', data={'letra': 'g'})
         json_data = response.get_json()
@@ -55,7 +60,7 @@ class Test(unittest.TestCase):
         self.assertIn('g', json_data['erradas'])
 
     def test_falla_letra_descuenta_vida(self):
-        # Compruebo que al ingresar una letra que no está en la palabra, pierdo una vida
+        """Test que verifica que se descuente una vida al falla una letra"""
         palabra = 'conejo'
         ahorcado = Ahorcado(palabra)
         letra = 'x'
@@ -63,6 +68,7 @@ class Test(unittest.TestCase):
         self.assertEqual(ahorcado.vidas, 5)
 
     def test_fin_de_vidas(self):
+        """Test que verifica el fin de vidas"""
         palabra = 'perro'
         ahorcado = Ahorcado(palabra)
         letras = ['x','y','z','w','t','q']
@@ -71,6 +77,7 @@ class Test(unittest.TestCase):
         self.assertEqual(ahorcado.vidas, 0)
 
     def test_fin_de_vidas_app(self):
+        """Test que verifica el fin de vidas desde la aplicación"""
         self.app.post('/configurar_palabra', data={'palabra': 'perro'})
         for letra in ['x', 'y', 'z', 'w', 't']:
             self.app.post('/ingresar_letra', data={'letra': letra})
@@ -80,7 +87,7 @@ class Test(unittest.TestCase):
         self.assertTrue(json_data['reiniciar'])
 
     def test_acierta_palabra_correcta(self):
-        # Compruebo que al ingresar una palabra intentando adivinar y acierto, gano el juego
+        """Test que verifica que se acierte la palabra correcta"""
         palabra = 'destino'
         ahorcado = Ahorcado(palabra)
         palabra_ingresada = 'destino'
@@ -88,13 +95,14 @@ class Test(unittest.TestCase):
         self.assertEqual(ahorcado.muestra_palabra(), 'destino')
 
     def test_acierta_palabra_correcta_app(self):
+        """Test que verifica que se acierte la palabra correcta desde la aplicación"""
         self.app.post('/configurar_palabra', data={'palabra': 'destino'})
         response = self.app.post('/arriesgar_palabra', data={'palabra_arriesga': 'destino'})
         json_data = response.get_json()
         self.assertTrue(json_data['resultado'])
 
     def test_falla_palabra_correcta(self):
-        # Compruebo que al ingresar una palabra intentando adivinar y fallo, pierdo una vida
+        """Test que verifica que falle la palabra correcta"""
         palabra = 'destino'
         ahorcado = Ahorcado(palabra)
         palabra_ingresada = 'ficticio'
@@ -102,7 +110,7 @@ class Test(unittest.TestCase):
         self.assertEqual(ahorcado.vidas, 5)
 
     def test_arriesgar_palabra_incorrecta(self):
-        # Compruebo que al ingresar una palabra intentando adivinar y fallo, pierdo una vida desde la aplicación
+        """Test que verifica que se arriesgue una palabra incorrecta"""
         self.app.post('/configurar_palabra', data={'palabra': 'python'})
         response = self.app.post('/arriesgar_palabra', data={'palabra_arriesga': 'java'})
         json_data = response.get_json()
@@ -110,7 +118,7 @@ class Test(unittest.TestCase):
         self.assertEqual(json_data['vidas'], 5)
 
     def test_muestra_palabra(self):
-        # Compruebo la funcionalidad de mostrar la palabra cuando se va adivinando
+        """Test que verifica que se muestre la palabra"""
         palabra_correcta = 'Gato'
         ahorcado = Ahorcado(palabra_correcta)
         for let in palabra_correcta:
@@ -118,7 +126,7 @@ class Test(unittest.TestCase):
         self.assertEqual(ahorcado.muestra_palabra(), 'Gato')
 
     def test_gana_juego(self):
-        # Compruebo que cuando gano la partida, el juego finaliza
+        """Test que verifica que se gane un juego"""
         palabra_correcta = 'Perro'
         ahorcado = Ahorcado(palabra_correcta)
         for let in palabra_correcta:
@@ -126,7 +134,7 @@ class Test(unittest.TestCase):
         self.assertTrue(ahorcado.comprueba_fin_de_juego())
 
     def test_guarda_letras_erradas(self):
-        # Compruebo que a medida que erro las letras, las mismas se van guardando
+        """Test que verifica que se guarden letras erradas"""
         palabra_correcta = 'pato'
         ahorcado = Ahorcado(palabra_correcta)
         letras = ['h','t','a','c','v']
@@ -135,11 +143,13 @@ class Test(unittest.TestCase):
         self.assertEqual(ahorcado.muestra_letras_erradas(),['h','c','v'])
 
     def test_verifica_caracter_especial(self):
+        """Test que verifica que no se ingresen caracteres especiales"""
         palabra = 'scrum'
         ahorcado = Ahorcado(palabra)
         self.assertFalse(ahorcado.ingresar_letra('?'))
 
     def test_verifica_numero(self):
+        """Test que verifica que no se ingresen números"""
         palabra = 'accesible'
         ahorcado = Ahorcado(palabra)
         self.assertFalse(ahorcado.ingresar_letra('5'))
@@ -152,7 +162,7 @@ class Test(unittest.TestCase):
         self.assertIn(ahorcado.uso_comodin(), ahorcado.palabra)
 
     def test_usar_comodin_app(self):
-        # Compruebo la funcionalidad de que cuando uso un comodín, este me devuelve una letra perteneciente a la palabra desde la aplicación
+        """Test que verifica que el comodin funcione bien en el flujo de aplicación"""
         self.app.post('/configurar_palabra', data={'palabra': 'ayuda'})
         response = self.app.post('/usar_comodin')
         json_data = response.get_json()
@@ -160,7 +170,6 @@ class Test(unittest.TestCase):
 
     def test_usar_dos_comodines(self):
         """Test que verifica que no se utilicen 2 comodines"""
-        # Compruebo que cuando intento usar 2 comodínes, el juego no me lo permite
         palabra = 'help'
         ahorcado = Ahorcado(palabra)
         ahorcado.uso_comodin()
